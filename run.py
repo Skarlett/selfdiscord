d7681e6198fb784f076d5ca90a52c8c0efa0a369
Skarlett <zykl@protonmail.com>
2019-11-12 20:33:52 -0600

    added inf loop
    Changes to be committed:
    	modified:   run.py

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import settings
 import logging
 
 if __name__ == "__main__":
-    logging.info("Starting...")
-    client = SelfBot(settings.PREFIX)
-    with open('token.secret') as fd:
-        token = fd.read().strip()
-    
-    client.run(token, bot=False)
+    while True:
+        logging.info("Starting...")
+        client = SelfBot(settings.PREFIX)
+        with open('token.secret') as fd:
+            token = fd.read().strip()
+        
+        client.run(token, bot=False)
